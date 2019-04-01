29ffe71e218884c738596bfa4c20c15a100b82fb
vas_code <vasco.1019@gmail.com>
2019-03-31 22:39:45 -0500

    show data in tabla

--- web2/src/helpers.js
+++ web2/src/helpers.js
@@ -3,3 +3,25 @@ const hbs = require('hbs');
 hbs.registerHelper('obtenerPromedio', (nota1,nota2,nota3) => {
     return (nota1+nota2+nota3)/3
 })
+
+hbs.registerHelper('listar',() => {
+    listaEstudiantes = require('./listado.json');
+    let texto = "<table> \
+                    <thead> \
+                    <th>Nombre</th> \
+                    <th>Matematicas</th> \
+                    <th>Ingles</th> \
+                </thead> \
+                <tbody>";
+
+    listaEstudiantes.forEach(estudiante => {
+        texto = texto +
+        '<tr>' +
+        '<td>'+ estudiante.nombre + '<td>' +
+        '<td>' + estudiante.matematicas + '<td>' +
+        '<td>' + estudiante.ingles + '</td></tr>' ;
+
+    })
+    texto = texto + '</tbody></table>';
+    return texto;
+});

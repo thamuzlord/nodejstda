0bee9ebfbd8475c0ce58f861529df4d78a74642f
vas_code <vasco.1019@gmail.com>
2019-03-31 23:00:45 -0500

    bootstrap

--- web2/src/app.js
+++ web2/src/app.js
@@ -2,12 +2,19 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const hbs = require('hbs');
+const bodyParser = require('body-parser');
 require('./helpers');
  
+const dirNode_modules = path.join(__dirname , '../node_modules')
 const directoriopublico = path.join(__dirname,'../public');
 const directoriopartials = path.join(__dirname,'../partials');
 app.use(express.static(directoriopublico));
 hbs.registerPartials(directoriopartials);
+app.use(bodyParser.urlencoded({extended:false}));
+app.use('/css', express.static(dirNode_modules + '/bootstrap/dist/css'));
+app.use('/js', express.static(dirNode_modules + '/jquery/dist'));
+app.use('/js', express.static(dirNode_modules + '/popper.js/dist'));
+app.use('/js', express.static(dirNode_modules + '/bootstrap/dist/js'));
 
 app.set('view engine', 'hbs');
 
@@ -17,16 +24,22 @@ app.get('/',(req,res)=>{
     })
 });
 
-app.get('/calculos',(req,res)=>{
+app.post('/calculos',(req,res)=>{
     res.render('calculos',{
-        estudiante: req.query.nombre,
-        nota1: parseInt(req.query.nota1),
-        nota2: parseInt(req.query.nota2),
-        nota3: parseInt(req.query.nota3)
+        estudiante: req.body.nombre,
+        nota1: parseInt(req.body.nota1),
+        nota2: parseInt(req.body.nota2),
+        nota3: parseInt(req.body.nota3)
     });
 });
 
  
 app.listen(3000, () => {
     console.log('Escuchando en el puerto 3000');    
+});
+
+app.get('*',(req,res) => {
+    res.render('error', {
+        estudiante: 'error'
+    });
 });

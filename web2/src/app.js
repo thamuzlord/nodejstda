fcb62564baceb830fadd23829616e7e2f2721437
vas_code <vasco.1019@gmail.com>
2019-03-31 21:59:11 -0500

    web2 mostrar nombre dinamicamente

--- web2/src/app.js
+++ web2/src/app.js
@@ -4,7 +4,9 @@ const path = require('path');
 const hbs = require('hbs');
  
 const directoriopublico = path.join(__dirname,'../public');
+const directoriopartials = path.join(__dirname,'../partials');
 app.use(express.static(directoriopublico));
+hbs.registerPartials(directoriopartials);
 
 app.set('view engine', 'hbs');
 
@@ -12,6 +14,12 @@ app.get('/',(req,res)=>{
     res.render('index', {
         estudiante: 'Vasco'
     })
+});
+
+app.get('/calculos',(req,res)=>{
+    res.render('calculos',{
+        estudiante: 'Iza'
+    });
 });
 
  

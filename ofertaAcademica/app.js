f6ac430a0c8278675941f169bf18931cc7b2ad1e
vas_code <vasco.1019@gmail.com>
2019-05-19 22:45:36 -0500

    Carga ejercicio 1

--- ofertaAcademica/app.js
+++ ofertaAcademica/app.js
@@ -1,7 +1,6 @@
 const {cursos} = require ('./datos.js');
 const fs = require('fs');
 
-
 const inscripcionCursos = {
 	id:{
 		demand: true,
@@ -18,8 +17,6 @@ const inscripcionCursos = {
 }
 
 const argv = require ('yargs').command('inscribirme','Realizar inscripción',inscripcionCursos).argv;
-const express = require('express');
-var app = express();
 
 let mostrarCurso=(pos, callback)=>{
 	setTimeout(function(){
@@ -39,30 +36,20 @@ function mostrarCursos() {
 
 const validarCurso = idCurso => {
 	let idExiste = cursos.find(function(curso) {
-	 return curso.id == idCurso})		
-
+	 return curso.id == idCurso})
 	 if(idExiste){
 		let nombreCurso = cursos.find(function(curso){
 			return curso.id == argv.id;
-		});
-		
+		});		
 		texto = 'El estudiante ' + argv.nombre + " con cedula " + argv.cedula + " se ha matriculado en el curso llamado " + nombreCurso.nombre + " con una duración de: " + nombreCurso.duracion + " horas y un valor de: " + nombreCurso.valor;
 		fs.writeFile('inscripcion.txt',texto,(err) => {
 			if(err) throw (err);
 			console.log("Archivo creado");
-		});
-
-		
+		});		
 	 }else{
 		console.log("El curso seleccionado no existe");
 	 }
 }
-
-app.get('/', function (req, res) {
-	res.send("texto")
-  });
-   
-app.listen(3000);
 
 mostrarCursos();
 validarCurso(argv.id);
